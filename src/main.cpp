c07f75f12e14640b483ca9f5151b01018a299ab1
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-06-23 11:48:43 +0800

    vscode

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
-﻿#include<CLI/CLI.hpp>
-#include<iostream>
-#include<Point24Solver.h>
-#include<stdint.h>
+﻿#include <CLI/CLI.hpp>
+#include <Point24Solver.h>
+#include <iostream>
+#include <stdint.h>
 
 using namespace std;
 
 int main(int argc, char **argv)
 {
-	CLI::App app { "输入 4 个数，求解 24 点。" };
+	CLI::App app{"输入 4 个数，求解 24 点。"};
 	std::vector<int> cli_numbers; // 用于存储解析后的整数
 
 	try
@@ -16,28 +16,27 @@ int main(int argc, char **argv)
 		auto opt = app.add_option_function<std::vector<int>>(
 			"--numbers",
 			[&](std::vector<int> value)
-		{
-			if (value.size() < 4)
 			{
-				throw CLI::ValidationError { "必须输入 4 个数。" };
-			}
+				if (value.size() < 4)
+				{
+					throw CLI::ValidationError{"必须输入 4 个数。"};
+				}
 
-			if (value.size() > 4)
-			{
-				value.resize(4);
-			}
+				if (value.size() > 4)
+				{
+					value.resize(4);
+				}
 
-			cli_numbers = value;
-		},
-			"输入一系列的数字"
-		);
+				cli_numbers = value;
+			},
+			"输入一系列的数字");
 
 		// 设置选项允许接受多个值（默认行为）
 		opt->allow_extra_args(true);
 		app.parse(argc, argv);
 
 		// 解析完成
-		std::array<uint8_t, 4> nums { };
+		std::array<uint8_t, 4> nums{};
 		for (uint32_t i = 0; i < nums.size(); i++)
 		{
 			nums[i] = cli_numbers[i];
