d42951b9b2044390827cc203d14b1c590ac35555
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-08-11 16:14:16 +0800

    clang-format

--- src/Point24Solver.h
+++ src/Point24Solver.h
@@ -6,18 +6,14 @@
 
 class Point24Solver
 {
-	static std::string OperatorToString(uint8_t op);
-	static std::string GetExpression(
-		std::array<uint8_t, 4> const &nums,
-		std::vector<uint8_t> const &operators);
-	static std::string TryAllOperatorCombinations(std::array<uint8_t, 4> const &nums);
-	static double CalculateExpression(std::string const &expr);
+    static std::string OperatorToString(uint8_t op);
+
+    static std::string GetExpression(std::array<uint8_t, 4> const &nums,
+                                     std::vector<uint8_t> const &operators);
+
+    static std::string TryAllOperatorCombinations(std::array<uint8_t, 4> const &nums);
+    static double CalculateExpression(std::string const &expr);
 
 public:
-	/// <summary>
-	///
-	/// </summary>
-	/// <param name="nums"></param>
-	/// <returns></returns>
-	static std::string Solve(std::array<uint8_t, 4> const &nums);
+    static std::string Solve(std::array<uint8_t, 4> const &nums);
 };
